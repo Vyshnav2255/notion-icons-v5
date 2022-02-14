279eac9b44032e2fcd42ae49a040736766f90154
mo-shaz <mhmmdshaz98@gmail.com>
2022-02-14 12:11:25 +0530

    + node()

--- assets/js/filter.js
+++ assets/js/filter.js
@@ -1,28 +1,28 @@
 
 
-    // Get all the svgs on the page
-    const svgs = document.getElementsByClassName("feather")
+// Get all the svgs on the page
+const svgs = document.getElementsByClassName("feather")
 
-    const changeColor = (color) => {
+const changeColor = (color) => {
+        
+        // Iterate through the svgs and change the stroke color
+        for(let i = 0; i < svgs.length; i++) {
             
-            // Iterate through the svgs and change the stroke color
-            for(let i = 0; i < svgs.length; i++) {
-                
-                    svgs[i].style.stroke = color
+                svgs[i].style.stroke = color
 
-                }
-        }
-
-
-
-      // Funtion that generates the dataUrl
-      const generateUrl = (element) => {
-
-        const preData = (new XMLSerializer).serializeToString(element)
-        const dataUrl = `data:image/svg+xml ${encodeURIComponent(preData)}`
-
-        return dataUrl
+            }
     }
+
+
+
+  // Funtion that generates the dataUrl
+  const generateUrl = (element) => {
+
+    const preData = (new XMLSerializer).serializeToString(element.node())
+    const dataUrl = `data:image/svg+xml ${encodeURIComponent(preData)}`
+
+    return dataUrl
+}
 
 // Function that downloads the SVG
 const downloadIcon = async (e) => {
